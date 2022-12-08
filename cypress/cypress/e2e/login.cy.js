964ec0b47401c2d99ad78ef53d4f4f850c6ee79a
Ladislav Slezák <lslezak@suse.cz>
2022-12-08 18:55:51 +0100

    Fixtures + skip login test

--- cypress/cypress/e2e/login.cy.js
+++ cypress/cypress/e2e/login.cy.js
@@ -1,31 +1,34 @@
 /// <reference types="cypress" />
 
-describe('The login page', () => {
-  beforeEach(() => {
-    cy.main_page();
-  });
+if (!Cypress.env('SKIP_LOGIN')) {
+  describe('The login page', () => {
+    beforeEach(() => {
+      cy.main_page();
+    });
 
-  it('allows root to login', () => {
-    let login = Cypress.env('LOGIN_USER') || 'root';
-    let password = Cypress.env('LOGIN_PASSWORD') || 'linux';
+    it('allows root to login', () => {
 
-    // wait until the login form is visible
-    cy.get('#login-user-input').should('be.visible').type(login);
-    cy.get('#login-password-input').type(password);
-    cy.get('#login-button').click();
+      let login = Cypress.env('LOGIN_USER') || 'root';
+      let password = Cypress.env('LOGIN_PASSWORD') || 'linux';
 
-    // the login button disappears
-    cy.get('#login-button').should('not.exist');
+      // wait until the login form is visible
+      cy.get('#login-user-input').should('be.visible').type(login);
+      cy.get('#login-password-input').type(password);
+      cy.get('#login-button').click();
+
+      // the login button disappears
+      cy.get('#login-button').should('not.exist');
+    })
+
+    it('reports an error for a wrong password', () => {
+      // wait until the login form is visible
+      cy.get('#login-user-input').should('be.visible').type('root');
+      cy.get('#login-password-input').type('wrong-password');
+      cy.get('#login-button').click();
+
+      // an error is reported, use longer timeout because unsuccessful login
+      // takes more time
+      cy.get('#login-error-message', { timeout: 10000 }).should('not.be.empty');
+    })
   })
-
-  it('reports an error for a wrong password', () => {
-    // wait until the login form is visible
-    cy.get('#login-user-input').should('be.visible').type('root');
-    cy.get('#login-password-input').type('wrong-password');
-    cy.get('#login-button').click();
-
-    // an error is reported, use longer timeout because unsuccessful login
-    // takes more time
-    cy.get('#login-error-message', { timeout: 10000 }).should('not.be.empty');
-  })
-})
+}
